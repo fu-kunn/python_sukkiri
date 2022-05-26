97745804379c05204c7a269aa96c8659c4767094
fu-kunn <sky53908@gmail.com>
2022-05-26 09:42:40 +0000

    [練習2-2]

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-weight = int(input("体重(kg)を入力"))
-height = float(input("身長(m)を入力"))
+scores = []
 
-bmi = weight / height / height
-print("BMIは{}です".format(bmi))
+scores.append(int(input("国語の点数>>")))
+scores.append(int(input("算数の点数>>")))
+scores.append(int(input("理科の点数>>")))
+scores.append(int(input("社会の点数>>")))
+scores.append(int(input("英語の点数>>")))
+
+print(f"合計{sum(scores)}点 / 平均{sum(scores) / len(scores)}点")
