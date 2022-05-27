9d8b4cb7e3540228c44b1a29393c73e4551f3d5a
fu-kunn <sky53908@gmail.com>
2022-05-27 08:16:21 +0000

    [練習3-3 (1)]

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-players1 = {"野球", "サッカー", "ラグビー", "ゴルフ", "水泳"}
-players2 = {"サッカー", "ソフトボール", "バスケ", "バトミントン", "スノボー"}
+isError = False
+n = 99
 
-input("心の準備ができたらEnterキーを押してください")
-
-common = players1 & players2
-total = players1 | players2
-
-compatibility_rate = len(common) / len(total) * 100
-print(f"相性度は{compatibility_rate}パーセントでした")
+if isError == False and n < 100:
+    print("表示")
