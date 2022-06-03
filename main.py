0a8e2a3630e95c0793dcbdb6afc8827eee0a7632
fu-kunn <sky53908@gmail.com>
2022-06-03 06:39:49 +0000

    [練習4-4 (3)]

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ for i in range(9):
     if (i + 1) % 2 == 0:
         continue
     for j in range(9):
+        if (i+1)*(j+1) > 50:
+            break
         print("{}×{}={}" .format(i+1, j+1, (i+1)*(j+1)))
