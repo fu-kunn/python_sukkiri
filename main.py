7cb9227f946a4f20965ae12f2f13c79f97035416
fu-kunn <sky53908@gmail.com>
2022-06-05 14:52:24 +0000

    [練習4-6]

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-for i in range(9):
-    if (i + 1) % 2 == 0:
-        continue
-    for j in range(9):
-        if (i+1)*(j+1) > 50:
-            break
-        print("{}×{}={}" .format(i+1, j+1, (i+1)*(j+1)))
+numbers = [1, 1]
+date = sum(numbers)
+count = 2
+while date <= 1000:
+    numbers.append(date)
+    date = date + numbers[count-1]
+    count += 1
+print(numbers)
