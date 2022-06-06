71a3bdd3d0f4c9dc20fe8b62cbb33d05f9a37419
fu-kunn <sky53908@gmail.com>
2022-06-06 09:17:43 +0000

    [練習5−５ 回答]

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-amount = int(input('支払総額を入力してください'))
-pepole= int(input('参加人数を入力してください'))
+def int_input(msg):
+    return int(input('{}を入力してください>>' .format(msg)))
 
-dnum = amount / pepole
-pay = dnum // 100 * 100
-if dnum > pay:
-    pay = int(pay + 100)
+def calc_payment(amount, pepole=2):
+    dnum = amount / pepole
+    pay = dnum // 100 * 100
+    if dnum > pay:
+        pay = int(pay + 100)
+    payorg = amount - pay * (pepole - 1)
+    return [int(pay), int(payorg)]
 
-payorg = amount - pay * (pepole - 1)
+def show_payment(pay, payorg, pepole=2):
+    print('***支払総額***')
+    print('一人当たり{}円({}人)、幹事は{}円です' .format(pay, pepole - 1, payorg))
 
-
-print('***支払総額***')
-print('一人当たり{}円({}人)、幹事は{}円です' .format(pay, pepole - 1, payorg))
+amount = int_input('支払総額'); pepole = int_input('参加人数')
+[pay, payorg] = calc_payment(amount, pepole)
+show_payment(pay, payorg, pepole)
