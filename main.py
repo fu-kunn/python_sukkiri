b50e31f3479efd1ce97bc00762fac973a9780404
fu-kunn <sky53908@gmail.com>
2022-06-07 07:11:47 +0000

    [練習6−２]

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-def int_input(msg):
-    return int(input('{}を入力してください>>' .format(msg)))
+x = ['ABC']
+y = [input('>>')]
 
-def calc_payment(amount, pepole=2):
-    dnum = amount / pepole
-    pay = dnum // 100 * 100
-    if dnum > pay:
-        pay = int(pay + 100)
-    payorg = amount - pay * (pepole - 1)
-    return [int(pay), int(payorg)]
+print(x[0] == y[0])
+print(id(x[0]) == id(y[0]))
 
-def show_payment(pay, payorg, pepole=2):
-    print('***支払総額***')
-    print('一人当たり{}円({}人)、幹事は{}円です' .format(pay, pepole - 1, payorg))
-
-amount = int_input('支払総額'); pepole = int_input('参加人数')
-[pay, payorg] = calc_payment(amount, pepole)
-show_payment(pay, payorg, pepole)
+y = x
+y[0] = 'XYZ'
+print(x[0])
